70c13075af3025c567a3fef504ef966f0d060d71
NeethuKPushkaran <141960227+NeethuKPushkaran@users.noreply.github.com>
2024-04-01 10:05:14 +0530

    'first-commit'

---
+++ Oops Introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//class Car
+//{
+//    string color = "red";
+
+//    static void Main(string[] args)
+//    {
+//        //Multiple Objects
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//        Console.WriteLine(car1.color);
+//        Console.WriteLine(car2.color);
+//    }
+//}
+
+
+class Car
+{
+    public string color = "red";
+}
+
+class Program
+{
+    static void Main(string[] args) { 
+        Car car = new Car();
+        Console.WriteLine(car.color);
+    }
+}
